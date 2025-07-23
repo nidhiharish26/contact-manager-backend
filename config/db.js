f0d7421246af0ddae659bbe6282b70e8c4766f6b
Nidhi Harish K <104716621+nidhiharish26@users.noreply.github.com>
2025-07-23 22:42:37 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,9 +6,9 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB Connected");
+    console.log("MongoDB Connected");
   } catch (error) {
-    console.error("❌ MongoDB connection error:", error.message);
+    console.error("MongoDB connection error:", error.message);
     process.exit(1); // Exit process with failure
   }
 };
